f6c61d0498db22bbadbca10ef3720857e348b336
Tung Pham Thanh <tung.pham1@ntq-solution.com.vn>
2022-11-01 10:31:16 +0700

    int

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+//
+import { marked } from "marked";
+
+const boxStyles = {
+  border: "1px solid black",
+  borderRadius: "4px",
+  padding: "40px 50px",
+};
+
+const defaultValue = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`;
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == \"\`\`\`\" && lastLine == \"\`\`\`\") {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
 
 function App() {
+  const [markdownValue, setMarkdownValue] = useState(defaultValue);
+
+  const handleChange = (e) => {
+    setMarkdownValue(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex" }}>
+      <div style={boxStyles}>
+        <div>Markdown Input</div>
+        <textarea id="editor" value={markdownValue} onChange={handleChange} />
+      </div>
+      <div style={boxStyles}>
+        <div>Markdown Preview</div>
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(markdownValue) }}
+        ></div>
+      </div>
     </div>
   );
 }
